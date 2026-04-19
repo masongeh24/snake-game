30a59d984222bcb9188233594810a8303118392a
BuildTools <unconfigured@null.spigotmc.org>
2026-04-19 12:16:19 -0500

    Created basic game panel, initializing snake and adding grid drawing

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,8 +1,12 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
-import java.awt.Dimension;
-import java.awt.Color;
 
 public class SnakeGame {
     public static void main(String[] args) {
@@ -22,8 +26,43 @@ public class SnakeGame {
     }
 
     private static class GamePanel extends JPanel {
+        private static final int CELL_SIZE = 30;
+        private static final int GRID_WIDTH = 20;
+        private static final int GRID_HEIGHT = 20;
+        private List<int[]> snake;
+
         public GamePanel() {
-            setBackground(Color.BLACK);
+            setBackground(Color.DARK_GRAY);
+            initializeSnake();
+        }
+
+        private void initializeSnake() {
+            snake = new ArrayList<>();
+            // Starting snake: three segments near center, facing right
+            snake.add(new int[]{10, 10}); // head
+            snake.add(new int[]{9, 10});  // body
+            snake.add(new int[]{8, 10});  // tail
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+
+            // Draw grid lines
+            g2d.setColor(Color.GRAY);
+            for (int i = 0; i <= GRID_WIDTH; i++) {
+                g2d.drawLine(i * CELL_SIZE, 0, i * CELL_SIZE, GRID_HEIGHT * CELL_SIZE);
+            }
+            for (int i = 0; i <= GRID_HEIGHT; i++) {
+                g2d.drawLine(0, i * CELL_SIZE, GRID_WIDTH * CELL_SIZE, i * CELL_SIZE);
+            }
+
+            // Draw snake
+            g2d.setColor(Color.GREEN);
+            for (int[] segment : snake) {
+                g2d.fillRect(segment[0] * CELL_SIZE, segment[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+            }
         }
     }
 }
